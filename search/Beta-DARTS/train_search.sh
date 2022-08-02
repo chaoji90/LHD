d3f54d448409e34cb1dfaaec3e2e1d7dfa512600
chaoji90 <aiencai@126.com>
2022-08-02 23:34:21 +0800

    add beta-darts implementation on LHD

---
+++ search/Beta-DARTS/train_search.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 ./$1 --dataset $2 --batch_size 128
+python3 ./$1 --dataset $2 --batch_size 128
+python3 ./$1 --dataset $2 --batch_size 128
+python3 ./$1 --dataset $2 --batch_size 128
+python3 ./$1 --dataset $2 --batch_size 128
